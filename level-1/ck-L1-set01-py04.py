cc96777f63f4949f9c9f6a52ab8e130506dd63b6
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-02-22 10:03:03 +0530

    Add files via upload

---
+++ level-1/ck-L1-set01-py04.py
@@ -0,0 +1,8 @@
+#check vowel or consonant
+import sys, string
+alpha = string.ascii_uppercase+string.ascii_lowercase
+#print(s)
+c = input('enter a char : ')
+if c in alpha : print('Alphabet')
+else  :  print('No')
+
