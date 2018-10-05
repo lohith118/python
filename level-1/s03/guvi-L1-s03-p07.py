2df92c5744d27e812de95b865e5d24db3c411425
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-05 19:37:51 +0530

    Update guvi-L1-s03-p07.py

--- level-1/s03/guvi-L1-s03-p07.py
+++ level-1/s03/guvi-L1-s03-p07.py
@@ -5,13 +5,13 @@ if s[0] == '+' or s[0] == '-' :
 if s[0] == '.' :
     for i in range(1,len(s)) :
         if not s[i].isdigit() :
-            print('N0')
+            print('No')
             sys.exit()
 else :
     i=0
     while  i<len(s) and s[i] != '.' :
         if not s[i].isdigit() :
-            print('N0')
+            print('No')
             sys.exit()
         i += 1
     i += 1
