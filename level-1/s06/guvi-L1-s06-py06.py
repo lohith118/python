f1723ecb3d7c8cedc91fbe7ea266648b53e2a15e
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-07 10:44:19 +0530

    Update guvi-L1-s06-py06.py

--- level-1/s06/guvi-L1-s06-py06.py
+++ level-1/s06/guvi-L1-s06-py06.py
@@ -4,8 +4,8 @@ a,n = 0,0
 for c in s :
     if c.isalpha() : a += 1
     if c.isdigit() : n += 1
-if a and n : print('yes')
-else :       print('no')
+if a and n : print('Yes')
+else :       print('No')
 
 
 
