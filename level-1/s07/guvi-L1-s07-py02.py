260f2f45dfe45c91d8e0602a298a7aaf190c95f2
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-07 12:48:14 +0530

    Update guvi-L1-s07-py02.py

--- level-1/s07/guvi-L1-s07-py02.py
+++ level-1/s07/guvi-L1-s07-py02.py
@@ -1,7 +1,15 @@
 import sys, string
-s = input()
-if ( s.count('1') + s.count('0') ) == len(s) : print('yes')
-else :                                         print('no')
+n = int(input())
+flag = 1
+while n :
+    a = n%10
+    if not ( a == 0 or a == 1) :
+        flag = 0
+        break
+    n //= 10
+if flag : print('yes')
+else :    print('no')
+
 
 
 
