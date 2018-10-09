3ac86e7e6dd60162b165a2ffa2e30fb8282baae2
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-09 15:41:05 +0530

    Update guvi-L3-s01-py04.py

--- Level-3/s01/guvi-L3-s01-py04.py
+++ Level-3/s01/guvi-L3-s01-py04.py
@@ -1,5 +1,7 @@
 import sys, string, math
 n = int(input())
 L = list(map(int,input().split()))
-for i in L :
-    if L.count(i) == 1 : print(i)
+for i in range(len(L)) :
+    if L.count(L[i]) == 1 :
+        print(L[i])
+        break
