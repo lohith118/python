50955bb0ac29db25ebce16c4dc36c2a933ccd868
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-10 13:18:05 +0530

    Add files via upload

---
+++ Level-3/s02/guvi-L3-s02-py10.py
@@ -0,0 +1,17 @@
+import sys, string, math, itertools
+def isprime(n) :
+    if n == 1 : return False
+    if n == 2: return True
+    for i in range(2,n) :
+        if n%i == 0 : return False
+    return True
+
+a,b = map(int,input().split())
+cnt = 0
+for i in range(a,b+1) :
+    s = bin(i)[2:]
+    k = s.count('1')
+    if isprime(k) : cnt += 1
+print(cnt)
+
+
