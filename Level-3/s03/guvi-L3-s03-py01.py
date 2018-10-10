3f457bd54de79d9214453f0710aa4d3535262d76
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-10 13:49:22 +0530

    Update guvi-L3-s03-py01.py

--- Level-3/s03/guvi-L3-s03-py01.py
+++ Level-3/s03/guvi-L3-s03-py01.py
@@ -10,5 +10,7 @@ for i in range(0,n) :
     if L[i].count(0) > 0 :
         L[i] = [0 for x in range(m)]
 for i in range(0,n) :
-    print(*L[i])
+    for j in range(0,m-1) :
+        print(L[i][j],end=' ')
+    print(L[i][m-1])
 
