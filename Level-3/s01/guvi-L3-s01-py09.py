c7cd23248b76f4c7b549eb70978e394d4b6512a0
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-10 08:53:41 +0530

    Update guvi-L3-s01-py09.py

--- Level-3/s01/guvi-L3-s01-py09.py
+++ Level-3/s01/guvi-L3-s01-py09.py
@@ -3,8 +3,17 @@ n = int(input())
 L = list(map(int,input().split()))
 L2 = []
 len1 = len(L)
+min1 = abs(L[0]+L[1])
+if min1 == 0:
+    print(L[0],L[1])
+    sys.exit()
 for i in range(0,len1-1) :
     for j in range(i+1,len1) :
-        if L[i]+L[j] == 0 :
-            print(L[i],L[j])
+        if abs(L[i]+L[j]) < min1 :
+            min1 = abs(L[i]+L[j])
+            a,b = L[i],L[j]
+            if min1 == 0 :
+                print(a,b)
+                sys.exit()
+print(a,b)
 
