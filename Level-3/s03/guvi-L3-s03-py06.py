fd78f703bc86a1f4febd1b4134c3eb957768c7fc
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-12 13:29:16 +0530

    Update guvi-L3-s03-py06.py

--- Level-3/s03/guvi-L3-s03-py06.py
+++ Level-3/s03/guvi-L3-s03-py06.py
@@ -1,8 +1,7 @@
-import sys,string
 n = int(input())
 L = [ int(x) for x in input().split()]
 L2 = L[::-1]
 k = len(L2)
 for i in range(0,k-1) :
     print(L2[i],end='->')
-print(L[k-1])
+print(L2[k-1])
