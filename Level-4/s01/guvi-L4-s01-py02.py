bc78633a0501688fa0042c7b3da29ad3735fe3d9
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-12 09:36:34 +0530

    Update guvi-L4-s01-py02.py

--- Level-4/s01/guvi-L4-s01-py02.py
+++ Level-4/s01/guvi-L4-s01-py02.py
@@ -19,10 +19,14 @@ if k <= i1 :
     s2 = ''.join(L)
     print(s2)
 else :
-    L2 = L[i1:]
-    L = L2[:]
+    L = L[i1:]
     k -= i1
-    #print(L2)
+    L2 = L[1:]
+    min1 = min(L2)
+    print(L,L2,k,min1)
+    i1 = L2.index(min1)
+    L2 = L[1:i1+1]
+    print(L2)
     while k :
         max1 = max(L2)
         #print(max1)
