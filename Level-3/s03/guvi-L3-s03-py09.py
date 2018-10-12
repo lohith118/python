cc30e6722599b06e5a53e453918cbe86a6628e6c
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-12 15:35:31 +0530

    Update guvi-L3-s03-py09.py

--- Level-3/s03/guvi-L3-s03-py09.py
+++ Level-3/s03/guvi-L3-s03-py09.py
@@ -1,16 +1,16 @@
 import sys,string
 n = int(input())
 L = [ int(x) for x in input().split()]
-sum2 = sum(L)
+s2 = 0
+for i in range(0,n) :
+    s2 = s2 + L[i]
 
 for j in range(n-2,-1,-1) :
-    #print('arr len = ', j+1)
     for i in range(0,n-j) :
         li, ri = i,j+i
-        sum1 = sum(L[li:ri + 1])
-        #print(li, ri, sum1)
-        if sum1 > sum2 :
-            sum2 = sum1
-print(sum2)
+        sum1 = sum(L[li:ri+1])
+        if sum1 > s2 :
+            s2 = sum1
+print(s2)
 
 
