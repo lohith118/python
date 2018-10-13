8f9503a5a5021092b2444ed4c0c95395c273dd77
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-13 13:50:21 +0530

    Update guvi-L3-s04-py04.py

--- Level-3/s04/guvi-L3-s04-py04.py
+++ Level-3/s04/guvi-L3-s04-py04.py
@@ -7,11 +7,13 @@ L2 = [ ''.join(x) for x in L]
 L3 = list(set(L2))
 #print(L3)
 L4 = [ int(x) for x in L3]
+if len(L4) == 1 :
+    print('impossible')
+    sys.exit()
 n = int(s)
-L4.remove(n)
 #print(L4)
 max1 = max(L4)
-if max1 < n :
+if max1 <= n :
     print('impossible')
     sys.exit()
 for i in range(0,len(L4)) :
@@ -19,5 +21,4 @@ for i in range(0,len(L4)) :
         if L4[i] < max1 :
             max1 = L4[i]
 print(max1)
-
 
