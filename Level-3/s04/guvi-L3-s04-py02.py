0a0e92a34a4db131bbdee20cdaac6e88024a005d
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-13 12:58:10 +0530

    Add files via upload

---
+++ Level-3/s04/guvi-L3-s04-py02.py
@@ -0,0 +1,16 @@
+import sys,string, itertools
+
+n = int(input())
+L = [ int(x) for x in input().split()]
+Li = [i for i in range(0,len(L))]
+#print(L,Li)
+while len(L) > 1 :
+    L2 = []
+    L2i = []
+    for i in range(1,len(L),2) :
+        L2.append(L[i])
+        L2i.append(Li[i])
+    #print(L2,L2i)
+    L = L2[:]
+    Li = L2i[:]
+print(Li[0]+1)
