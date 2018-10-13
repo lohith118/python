31f3d918f2e04e9db0b1f8681b37bf747038643d
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-13 20:51:37 +0530

    Add files via upload

---
+++ Level-3/s05/guvi-L3-s05-py09.py
@@ -0,0 +1,16 @@
+import sys, string, math
+def fact(n) :
+    p = 1
+    if n == 0 : return 1
+    for i in range(1,n+1) :
+        p = p * i
+    return p
+
+n,k = input().split()
+n,k = int(n), int(k)
+print(n,k)
+ncr = fact(n)
+dn =  fact(n-k) * fact(k)
+ncr = ncr // dn
+print(ncr)
+
