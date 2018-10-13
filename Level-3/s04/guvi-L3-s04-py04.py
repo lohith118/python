7dc6d585083860f5ef4a20f0908d49e9c28cee00
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-13 13:44:40 +0530

    Add files via upload

---
+++ Level-3/s04/guvi-L3-s04-py04.py
@@ -0,0 +1,23 @@
+import sys,string
+from itertools import permutations
+
+s = input()
+L = list(permutations(s))
+L2 = [ ''.join(x) for x in L]
+L3 = list(set(L2))
+#print(L3)
+L4 = [ int(x) for x in L3]
+n = int(s)
+L4.remove(n)
+#print(L4)
+max1 = max(L4)
+if max1 < n :
+    print('impossible')
+    sys.exit()
+for i in range(0,len(L4)) :
+    if L4[i] > n :
+        if L4[i] < max1 :
+            max1 = L4[i]
+print(max1)
+
+
