86ae3eee156359488698c12059f1783f1e84f55a
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-15 13:30:44 +0530

    Add files via upload

---
+++ Level-3/s08/guvi-L3-s08-py04.py
@@ -0,0 +1,28 @@
+import sys,string
+
+def LCS_2(S,T):
+    m = len(S)
+    n = len(T)
+    counter = [[0]*(n+1) for x in range(m+1)]
+    longest = 0
+    L = []
+    for i in range(m):
+        for j in range(n):
+            if S[i] == T[j]:
+                c = counter[i][j] + 1
+                counter[i+1][j+1] = c
+                if c > longest:
+                    L = []
+                    longest = c
+                    L.append(S[i-c+1:i+1])
+                elif c == longest:
+                    L.append(S[i-c+1:i+1])
+
+    return L
+s1 = input()
+s2 = input()
+res = LCS_2(s1,s2)
+if len(res) == 1 :
+    print(*res)
+else :
+    print(max(res))
