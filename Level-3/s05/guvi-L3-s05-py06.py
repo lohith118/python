218ec3a2a2d7e200e4e21fdbd5571397649c6cf0
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-15 10:02:09 +0530

    Delete guvi-L3-s05-py06.py

--- Level-3/s05/guvi-L3-s05-py06.py
+++
@@ -1,9 +0,0 @@
-import sys,string, itertools
-n = int(input())
-L = [ int(x) for x in input().split()]
-cnt = 0
-for i in range(0,n) :
-    k = (i+1) * L[i]
-    if k in L :
-        cnt += 1
-print(cnt)
