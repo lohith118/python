c5dc6a1b29cbda953949742a6417ee15f912a555
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-15 13:02:25 +0530

    Update guvi-L3-s08-py03.py

--- Level-3/s08/guvi-L3-s08-py03.py
+++ Level-3/s08/guvi-L3-s08-py03.py
@@ -1,36 +1,23 @@
 import sys,string
-def LCS_1(s1,s2):
-    answer = ""
-    len1, len2 = len(s1), len(s2)
-    for i in range(len1):
-        match = ""
-        for j in range(len2):
-            if (i + j < len1 and s1[i + j] == s2[j]):
-                match += s2[j]
-            else:
-                if (len(match) > len(answer)): answer = match
-                match = ""
-    return answer
 
 def LCS_2(S,T):
     m = len(S)
     n = len(T)
     counter = [[0]*(n+1) for x in range(m+1)]
     longest = 0
-    lcs_set = set()
+    L = []
     for i in range(m):
         for j in range(n):
             if S[i] == T[j]:
                 c = counter[i][j] + 1
                 counter[i+1][j+1] = c
                 if c > longest:
-                    lcs_set = set()
+                    L = []
                     longest = c
-                    lcs_set.add(S[i-c+1:i+1])
+                    L.append(S[i-c+1:i+1])
                 elif c == longest:
-                    lcs_set.add(S[i-c+1:i+1])
-
-    res = ''.join(list(lcs_set))
+                    L.append(S[i-c+1:i+1])
+    res = ''.join(L)
     return res
 s1 = input()
 s2 = input()
