abeb11c97ea1ce3e6452891f12b31f1f1687ab8e
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-16 09:07:54 +0530

    Add files via upload

---
+++ Level-3/s09/guvi-L3-s09-py03.py
@@ -0,0 +1,12 @@
+import sys,string
+s = input()
+print(s)
+for i in range(11,100,11) :
+    s2 = str(i)
+    if s2 in s :
+        i2 = s.index(s2)
+        s3 = s[:i2] + s2[0] + s[i2+2:]
+        print(s3)
+        break
+
+
