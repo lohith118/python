ab1bbca7783a72a73a836b2acb2871f90c10aa08
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-16 09:27:22 +0530

    Add files via upload

---
+++ Level-3/s09/guvi-L3-s09-py04.py
@@ -0,0 +1,25 @@
+import sys,string
+s = input()
+n = len(s)
+s2 = ''
+m = n
+i=0
+print(s)
+#aabbaa
+while m > 0:
+    k=0
+    c = s[i]
+    while i<n and s[i] == c :
+        k += 1
+        i += 1
+    if k>1 :
+        s2 = s2 + c + '*' + str(k)
+        m -= k
+    else :
+        s2 = s2 + c
+        m -= 1
+print(s2)
+
+
+
+
