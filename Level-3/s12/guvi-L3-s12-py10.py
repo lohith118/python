bebfa0b8b8e12362141065ba01516896e6f3ec36
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-17 15:49:53 +0530

    Add files via upload

---
+++ Level-3/s12/guvi-L3-s12-py10.py
@@ -0,0 +1,23 @@
+import sys,string
+
+n = int(input())
+L = [ int(x) for x in input().split()]
+cnt = 0
+for i in range(0,n-2) :
+    for j in range(i+1,n-1) :
+        for k in range(j+1,n) :
+            if L[i] + L[j] == L[k] :
+                cnt += 1
+print(cnt)
+
+
+
+
+
+
+
+
+
+
+
+
