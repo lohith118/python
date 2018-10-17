161c003bc6e82f4ae4a7f021f68aa98072c8cd33
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-17 15:24:43 +0530

    Add files via upload

---
+++ Level-3/s12/guvi-L3-s12-py05.py
@@ -0,0 +1,30 @@
+import sys,string
+
+def isPrime(n) :
+    if n==1 : return False
+    if n==2 or n==3 : return True
+    for i in range(2,n) :
+        if n%i == 0 :
+            return False
+    return True
+
+n = int(input())
+cnt = 0
+for i in range(2,n//2+1) :
+    if isPrime(i) and isPrime(n-i)  :
+        cnt += 1
+        #print(i,n-i,cnt)
+print(cnt)
+
+
+
+
+
+
+
+
+
+
+
+
+
