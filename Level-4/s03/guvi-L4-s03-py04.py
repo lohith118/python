9808c2be9a660d12c7762aab426ed07a9e730328
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-20 11:15:49 +0530

    Update guvi-L4-s03-py04.py

--- Level-4/s03/guvi-L4-s03-py04.py
+++ Level-4/s03/guvi-L4-s03-py04.py
@@ -1,13 +1,16 @@
 import sys, string, math
-from itertools import permutations, combinations
 n = int(input())
 k = 2**n
+L = []
 for i in range(0,k) :
     s = bin(i)[2:]
     j = len(s)
     if j < n :
         s = '0' * (n-j) + s
-    print(s)
+    L.append(s)
+for i in range(0,len(L)) :
+    print(L[i])
+
 
 
 
