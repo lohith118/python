b4509f449c5d8f9b980198fa8a10b75b28096b40
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-20 13:25:26 +0530

    Add files via upload

--- Level-4/s03/guvi-L4-s06-py01.py
+++ Level-4/s03/guvi-L4-s06-py01.py
@@ -1,34 +1,15 @@
 import sys, string, math
-
-n = int(input())
-L = [ int(x) for x in input().split()]
-n = len(L)
-L2 = []
-for i in range(0,n) :
-    cnt = 1
-    if L[i] > 0:
-        flag = 1
-    else:
-        flag = -1
-    for j in range(i+1,n) :
-        if L[j] > 0:
-            if flag == 1 :
-                break
-            else :
-                cnt += 1
-                flag = 1
-        else:
-            if flag == -1 :
-                break
-            else :
-                cnt += 1
-                flag = -1
-    L2.append(cnt)
-print(*L2)
-
-
-
-
-
+s1 = input()
+s2 = input()
+s3 = string.ascii_lowercase
+L = []
+for i in range(0, len(s1)) :
+    k = ord(s1[i]) + s3.index(s2[i]) + 1
+    if k > ord('a')+25 :
+        k -= 26
+    #print(k,chr(k))
+    L.append(chr(k))
+s4 = ''.join(L)
+print(s4)
 
 
