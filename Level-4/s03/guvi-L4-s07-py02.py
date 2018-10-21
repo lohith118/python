88ae4cb045639c8712845ec3fa9952902c68c222
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-21 06:58:03 +0530

    Add files via upload

---
+++ Level-4/s03/guvi-L4-s07-py02.py
@@ -0,0 +1,20 @@
+import sys, string, math
+def isPalin(s) :
+    if len(s) == 1 : return False
+    if s == s[::-1] : return True
+    return False
+
+s = input()
+n = len(s)
+for i in range(n-1,0,-1) :
+    for j in range(0,n-i) :
+        i1 = j
+        i2 = j+i+1
+        s2 = s[i1:i2]
+        #print(i1,i2-1,s2)
+        if isPalin(s2) :
+            #print(s2)
+            print(n-len(s2))
+            sys.exit()
+
+
