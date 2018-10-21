3ebe0b805da357e7fc8e19e574579c133486fa47
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-21 11:28:45 +0530

    Add files via upload

---
+++ Level-4/s01/guvi-L4-s02-py06.py
@@ -0,0 +1,11 @@
+import sys, string, math
+n = int(input())
+L = [ int(x) for x in input().split()]
+k = n
+L2 = [1]*n
+if L[0] > L[1] :
+    L2[0] += 1
+for i in range(1,n) :
+    if L[i] > L[i-1] :
+        L2[i] = L2[i-1] + 1
+print(sum(L2))
