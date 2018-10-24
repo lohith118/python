77904fbe05924b330a5737c4e83f7523eb3f3d6c
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-10-24 14:11:51 +0530

    Update guvi-L4-s05-py02.py

--- Level-4/s03/guvi-L4-s05-py02.py
+++ Level-4/s03/guvi-L4-s05-py02.py
@@ -3,15 +3,15 @@ import sys,string
 
 def maxOfSegmentMins(L, n, k):
     if k == 1:
-        return min(L);
-
+        return min(L)
     if k == 2 :
-        return max(L[0], L[n - 1]);
-
-    return max(L);
+        return max(L[0], L[n - 1])
+    return max(L)
 
 n,k = input().split()
 n,k = int(n),int(k)
 L = [ int(x) for x in input().split()]
 n = len(L)
-print(maxOfSegmentMins(L, n, k))
+ans = maxOfSegmentMins(L, n, k)
+print(ans)
+
