e8ef46f08b70d5b0b3bf34900465ef3ba780675b
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-02 13:37:49 +0530

    Update guvi-L4-s06-py09.py

--- Level-4/s03/guvi-L4-s06-py09.py
+++ Level-4/s03/guvi-L4-s06-py09.py
@@ -1,4 +1,5 @@
 import sys,string, math, itertools
+
 s1 = input()
 L = s1.split('|')
 s = input()
@@ -6,15 +7,21 @@ s = input()
 n1 = abs(len(L[0])-len(L[1]))
 #print(n1)
 if n1==0 :
-    print(s1)
-s2 = 'KST'
+    print('Impossible')
+    sys.exit()
+if n1 != len(s) :
+    print('Impossible')
+    sys.exit()
 if len(s) >= n1 :
     if len(L[0]) < len(L[1]) :
-        s2 = L[0] + s[:n1]+ '|' + L[1]
+        s2 = L[0] + s + '|' + L[1]
     else :
-        s2 = L[0] + '|' + L[1] + s[:n1]
+        s2 = L[0] + '|' + L[1] + s
     print(s2)
 else :
     print('Impossible')
+
+
+
 
 
