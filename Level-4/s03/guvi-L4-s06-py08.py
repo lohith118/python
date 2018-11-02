a86a4df8da044865549146023c5ad34dfc45eb91
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-02 13:07:52 +0530

    Update guvi-L4-s06-py08.py

--- Level-4/s03/guvi-L4-s06-py08.py
+++ Level-4/s03/guvi-L4-s06-py08.py
@@ -3,6 +3,7 @@ import sys,string, math, itertools
 n = int(input())
 L = []
 if n==2 :
+    print('3')
     print('2 1 2')
     sys.exit()
 k = n//2
@@ -12,6 +13,6 @@ for i in range(1,n,2 ) :
     L.append(i)
 for i in range(2,n+1,2 ) :
     L.append(i)
+print(len(L))
 print(*L)
-
 
