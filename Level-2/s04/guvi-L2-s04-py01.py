0c4e80c1e9bd4ed375acee7e1005e8df6c320128
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-03 09:19:41 +0530

    Add files via upload

---
+++ Level-2/s04/guvi-L2-s04-py01.py
@@ -0,0 +1,22 @@
+import sys,string, math,itertools
+
+s = input()
+b1=0
+n = len(s)
+if s[0]== ')' :
+    printf("No")
+    sys.exit()
+for i in range(0,n) :
+    if s[i] == '(' :
+        b1 += 1
+    if s[i] == ')' :
+        b1 -= 1
+    if b1<0 :
+        printf("No")
+        sys.exit()
+if b1==0 :
+    print("yes")
+else :
+    print("no")
+
+
