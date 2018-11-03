7640b5c82022a8648ab97e45a15f6e480aac9e99
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-03 14:51:21 +0530

    Update guvi-L2-s05-py05.py

--- Level-2/s05/guvi-L2-s05-py05.py
+++ Level-2/s05/guvi-L2-s05-py05.py
@@ -3,7 +3,7 @@ import sys,string, math,itertools
 n,k = input().split()
 p,a = int(n),int(k)
 for b in range(1,p) :
-    if 2 * (b + a//b) == p :
+    if 2 * (b + a/b) == p :
         print('yes')
         sys.exit()
 print('no')
