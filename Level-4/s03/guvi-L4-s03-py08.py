cbbf29ee45ddfe0392e6c85429efbd7ecbd8c366
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-09 10:55:33 +0530

    Update guvi-L4-s03-py08.py

--- Level-4/s03/guvi-L4-s03-py08.py
+++ Level-4/s03/guvi-L4-s03-py08.py
@@ -2,9 +2,6 @@ import sys, string, math
 
 n = int(input())
 L = [ int(x) for x in input().split()]
-if n==5 and L == [15, 2, 1, 5, 3] :
-    print(4)
-    sys.exit()
 L.sort()
 
 cnt = 0
@@ -12,6 +9,5 @@ for i in range(0,n) :
     #print(i+1, L[i], sum(L[:i]),L[i]-sum(L[:i]))
     if L[i]-sum(L[:i]) >= 0 :
         cnt += 1
-    else :
-        break
+
 print(cnt)
