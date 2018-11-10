e52cabfe06e4db440c443760be7b83bde5bb1eb1
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-10 13:16:38 +0530

    Add files via upload

---
+++ Level-2/s11/guvi-L2-s11-py05.py
@@ -0,0 +1,19 @@
+import sys,string, math,itertools
+
+n = int(input())
+L = [ int(x) for x in input().split()]
+L2 = sorted(L)
+L3 = []
+for x in L2 :
+    L3.append(L.index(x))
+print(*L3)
+
+
+
+
+
+
+
+
+
+
