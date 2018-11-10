0e8a57d6ceeb396c227b50fa1c552c83ceea7d98
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-10 13:11:50 +0530

    Add files via upload

---
+++ Level-2/s11/guvi-L2-s11-py04.py
@@ -0,0 +1,18 @@
+import sys,string, math,itertools
+
+n = int(input())
+L = [ int(x) for x in input().split()]
+sum1 = 0
+for i in range(0,n-1) :
+    sum1 += L[i] + L[i+1]
+print(sum1)
+
+
+
+
+
+
+
+
+
+
