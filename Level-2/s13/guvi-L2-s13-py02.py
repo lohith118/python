5b8c6a5126bd3204c2900382d7c279a95ebcbe8e
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-12 10:49:08 +0530

    Add files via upload

---
+++ Level-2/s13/guvi-L2-s13-py02.py
@@ -0,0 +1,14 @@
+import sys,string, math,itertools
+
+L = input().split('-')
+L2 = ['','January','February','March','April','May','June','July','August',
+      'September','October','November','December']
+k = int(L[1])
+print(L2[k])
+
+
+
+
+
+
+
