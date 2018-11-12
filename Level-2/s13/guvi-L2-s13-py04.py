314247c436eb99f49616e8150e821d099b263456
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-12 11:13:21 +0530

    Update guvi-L2-s13-py04.py

--- Level-2/s13/guvi-L2-s13-py04.py
+++ Level-2/s13/guvi-L2-s13-py04.py
@@ -2,6 +2,9 @@ import sys,string, math,itertools
 
 n = int(input())
 L = [ int(x) for x in input().split()]
+if L.count(1) == n :
+      print(1)
+      sys.exit()
 for x in range(max(L), max(L)**n,max(L)) :
       L2 = [ not x%i for i in L]
       #print(x,L2)
