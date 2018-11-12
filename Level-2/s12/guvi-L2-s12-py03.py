48f654143a748cb4fa7e034496c9b3ac9798ada8
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-12 09:14:54 +0530

    Add files via upload

---
+++ Level-2/s12/guvi-L2-s12-py03.py
@@ -0,0 +1,18 @@
+import sys,string, math,itertools
+
+L = input().split('/')
+#print(L)
+if len(L[0]) != 2 or  len(L[1]) != 2 or  len(L[2]) != 4 :
+    print('no')
+    sys.exit()
+
+
+
+
+
+
+
+
+
+
+
