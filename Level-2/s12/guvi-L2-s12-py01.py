5bb8fb603a942da9fd85672f12df2ce4490ed595
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-12 09:05:40 +0530

    Update guvi-L2-s12-py01.py

--- Level-2/s12/guvi-L2-s12-py01.py
+++ Level-2/s12/guvi-L2-s12-py01.py
@@ -6,15 +6,10 @@ L = [ int(x) for x in input().split()]
 L1 = L[:n]
 L2 = L[n:]
 L3 = []
-while len(L1) :
-    x = L1[0]
+for x in L1 :
     if x in L2 :
         L3.append(x)
-        L1.remove(x)
         L2.remove(x)
-    else :
-        L1.remove(x)
-
 L3.sort()
 print(*L3)
 
