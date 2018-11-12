22c17f43e185fb0634ba655cc09a951325a16f44
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-12 10:27:17 +0530

    Update guvi-L2-s12-py09.py

--- Level-2/s12/guvi-L2-s12-py09.py
+++ Level-2/s12/guvi-L2-s12-py09.py
@@ -1,8 +1,15 @@
 import sys,string, math,itertools
 
 s = input()
-ans = int(s,16)
-print(ans)
+n = len(s)
+sum1 = 0
+for c in s :
+    c = c.upper()
+    if c.isdigit() :
+        sum1 = sum1*16 + (ord(c) - ord('0'))
+    elif c >= 'A' and c <= 'F' :
+        sum1 = sum1*16 + (ord(c) - ord('A')+10)
+print(sum1)
 
 
 
