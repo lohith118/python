527e9916e745046b606f11a7047230f60ba817f3
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-13 16:38:52 +0530

    Update guvi-L2-s16-py05.py

--- Level-2/s16/guvi-L2-s16-py05.py
+++ Level-2/s16/guvi-L2-s16-py05.py
@@ -5,5 +5,5 @@ a,b = int(a),int(b)
 L1 = [ int(x) for x in input().split()]
 k = a//2
 
-L2 = sorted(L1[:k]) + sorted(L1[k:],reverse = True)
+L2 = sorted(L1[:b]) + sorted(L1[b:],reverse = True)
 print(*L2)
