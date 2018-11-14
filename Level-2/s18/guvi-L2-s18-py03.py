2db87a4052cf933abbaf4e917d77b1cc66c7ecc2
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-14 11:33:06 +0530

    Add files via upload

---
+++ Level-2/s18/guvi-L2-s18-py03.py
@@ -0,0 +1,25 @@
+import sys,string, math,itertools
+
+L1 = input().split()
+L2 = input().split()
+L3 = []
+if len(L1) == 1 :
+    L1 = list(L1[0])
+    L2 = list(L2[0])
+    n1 = len(L1)
+    n2 = len(L2)
+    k = min(n1, n2)
+    for i in range(0, k) :
+        if L1[i] != L2[i] :
+            L3.append(L1[i])
+            L3.append(L2[i])
+    if n1 > n2 :
+        L3.extend(L1[n2:])
+    elif n2 > n1:
+            L3.extend(L2[n1:])
+    print(*L3)
+else :
+    for s in L1 :
+        if s not in L2 :
+            L3.append(s)
+    print(*L3)
