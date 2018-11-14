92d82f91b9a4b9ee227806eb1f62c8a76a7a1e29
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-14 12:38:37 +0530

    Add files via upload

--- Level-4/s01/guvi-L4-s01-py02.py
+++ Level-4/s01/guvi-L4-s01-py02.py
@@ -1,35 +1,16 @@
-import sys,string
-def buildLowestNumber2(str1, n, res) :
-    if n == 0 :
-        res.append(str1);
-        return
 
-    len1 = len(str1);
-    if len1 <= n :  return;
+import sys, string, math
+def reduceN( n, k) :
+	if k <= 0 : return n
 
-    # Find the smallest char among first(n + 1) chars of str.
-    minc = min(str1[:n+1])
-    min1 = str1.index(minc)
-    #print('min1=',minc,min1)
-    # Append the smallest char to res -
-    res.append(str1[min1]);
-    #print(res)
+	if n == 0 : return 10	# Fail
+	p1 = reduceN(n//10, k)*10 + n%10
+	p2 = reduceN(n//10, k-1)
+	if p1 < p2 :
+		return p1
+	else :
+		return p2
 
-    # substring starting from minIndex +1 to str.length() - 1.
-    new_str = str1[min1+1 : ]
-    #print('s2=',new_str)
-
-    # Recur for the above substring and n equals to n-minIndex
-    buildLowestNumber2(new_str, n-min1, res);
-
-def buildLowestNumber(s1, n) :
-    res = []
-    buildLowestNumber2(s1, n,res)
-    return res
-
-# Driver program
-s1,n = input().split()
-n = int(n)
-L = buildLowestNumber(s1, n)
-ans = ''.join(L)
-print(ans)
+n,k = input().split()
+n,k = int(n),int(k)
+print(reduceN(n,k))
