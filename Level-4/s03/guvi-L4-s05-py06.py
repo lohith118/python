b6cc9f669656631fb0dd2baf1e0f6a08f09f137c
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-17 16:14:49 +0530

    Add files via upload

---
+++ Level-4/s03/guvi-L4-s05-py06.py
@@ -0,0 +1,23 @@
+import sys, string, math
+
+n = int(input())
+Lp = [0] + [ int(x) for x in input().split()]
+Lsum = [0 for i in range(0,n+4)]
+np = 2
+A = 0
+B = 1
+Lmax = []
+for i in range(0,n+1) :
+    Lmax.append([0,0])
+
+for i in range(n,0,-1) :
+    Lsum[i] = Lp[i] + Lsum[i+1]
+    if i == n :
+        Lmax[i][A] = Lmax[i][B] = Lp[i]
+    else :
+        Lmax[i][A] = max(Lmax[i + 1][A], Lp[i] + Lsum[i + 1] - Lmax[i + 1][B])
+        Lmax[i][B] = max(Lmax[i + 1][B], Lp[i] + Lsum[i + 1] - Lmax[i + 1][B])
+Amax = Lsum[1] - Lmax[1][B]
+print(Amax, Lmax[1][B])
+
+
