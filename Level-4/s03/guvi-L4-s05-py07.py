e75bb15c1716dea4d6b95e0cd5d940446b3cf325
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-19 18:25:37 +0530

    Add files via upload

---
+++ Level-4/s03/guvi-L4-s05-py07.py
@@ -0,0 +1,67 @@
+import sys, string, math
+
+def isPrime(n) :
+    if n <= 1 : return False
+    if n==2 or n==3 : return True
+    a = int(math.sqrt(n)+1)
+    for i in range(2,a+1) :
+        if n%i == 0 :
+            return False
+    return True
+
+def factors1(n) :
+    L = []
+    i = 2
+    while n >1 :
+        while n%i == 0 :
+            L.append(i)
+            n //= i
+        i += 1
+    return L
+n,k = input().split()
+n,k = int(n), int(k)
+if k==0 :
+    print(n)
+    sys.exit()
+a = 10**k
+if isPrime(n) :
+    print(n * a)
+    sys.exit()
+L = factors1(n)
+#print(L)
+cnt2 = L.count(2)
+cnt5 = L.count(5)
+#print(cnt2,cnt5)
+if cnt2 + cnt5 == 0 :
+    print(n * a)
+    sys.exit()
+if cnt5==0 :
+    p = 1
+    for x in L :
+        if x != 2 :
+            p = p * x
+    if cnt2 > k :
+        k = cnt2
+    print(p * 10**k)
+    sys.exit()
+if cnt2==0 :
+    p = 1
+    for x in L :
+        if x != 5 :
+            p = p * x
+    if cnt5 > k :
+        k = cnt5
+    print(p * 10**k)
+    sys.exit()
+if cnt2 > 0 and cnt5 > 0 :
+    if cnt2 > cnt5 :
+        if cnt2 > k :
+            k = cnt2
+    elif cnt5 > cnt2:
+        if cnt5 > k :
+            k = cnt5
+    p = 1
+    for x in L :
+        if x != 5 and x != 2 :
+            p = p * x
+    print(p * 10**k)
