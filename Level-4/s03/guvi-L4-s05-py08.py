7eb0b8bd4ffe94e0a9379c6b31f692e78149e412
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-19 20:51:29 +0530

    Add files via upload

---
+++ Level-4/s03/guvi-L4-s05-py08.py
@@ -0,0 +1,23 @@
+import sys, string, math
+
+def factors1(n) :
+    L = []
+    i = 2
+    while n >1 :
+        while n%i == 0 :
+            L.append(i)
+            n //= i
+        i += 1
+    return L
+
+n = int(input())
+L = [input().split() for i in range(0,n)]
+
+for i in range(0,n) :
+    p = 1
+    n, k = L[i]
+    n, k = int(n), int(k)
+    for i in range(k+1,n+1) :
+        p = p*i
+    L2 = factors1(p)
+    print(len(L2))
