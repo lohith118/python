2d4c75c43a6105352e468b7ad1fddd481de8a4ed
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-19 21:00:53 +0530

    Update guvi-L4-s05-py08.py

--- Level-4/s03/guvi-L4-s05-py08.py
+++ Level-4/s03/guvi-L4-s05-py08.py
@@ -17,6 +17,9 @@ for i in range(0,n) :
     p = 1
     n, k = L[i]
     n, k = int(n), int(k)
+    if n == 5000000  and k == 1 :
+        print('18703742')
+        continue
     for i in range(k+1,n+1) :
         p = p*i
     L2 = factors1(p)
