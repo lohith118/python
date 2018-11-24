bfcd5b59dde2f7903c508dacac43d9191a0f737f
ksthacker <36289700+ksthacker@users.noreply.github.com>
2018-11-24 08:59:43 +0530

    Update guvi-L2-s04-py05.py

--- Level-2/s04/guvi-L2-s04-py05.py
+++ Level-2/s04/guvi-L2-s04-py05.py
@@ -1,6 +1,5 @@
 import sys,string, math,itertools
 
-import sys, string
 def minChCnt(s) :
     dic1 = {}
     for c in s :
@@ -8,9 +7,7 @@ def minChCnt(s) :
             dic1[c] = dic1.get(c,0) + 1
     min1 = sys.maxsize
     L = []
-    for x in dic1.values() :
-        if x < min1 :
-            min1 = x
+    min1 = min(dic1.values())
     for k,v in dic1.items() :
         if v == min1 :
             L.append(k)
@@ -19,6 +16,5 @@ def minChCnt(s) :
 s = input()
 n = len(s)
 L = minChCnt(s)
-L3 = [x.lower() for x in L]
-print(*L3)
+print(*L)
 
